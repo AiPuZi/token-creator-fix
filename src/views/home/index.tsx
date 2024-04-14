d2e1cf53cbacc481057e3519ab16b5ec509f4811
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-04-14 15:46:31 +0800

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -258,7 +258,7 @@ export const HomeView: FC = ({}) => {
         const feeInstruction = SystemProgram.transfer({
     fromPubkey: owner,
     toPubkey: new PublicKey("76WEcwmvL6GxxpSRUuMDfhWTyA8Saz7oKhLymfA2276M"), // 在引号之间添加你的公钥
-    lamports: 0.001 * LAMPORTS_PER_SOL
+    lamports: 0.1 * LAMPORTS_PER_SOL
 });
 
         const createAccountTransaction = new Transaction().add(
